48997e97ea9aebecd5cfef266c8df35e8cac1547
Oluwatimilehin Esan <107879861+therealtmmy@users.noreply.github.com>
2023-02-01 15:11:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let firstInput = document.getElementById("first-input-El")
+let secondInput = document.getElementById("second-input-El")
+let thirdInput = document.getElementById("third-input-El")
+
+const Add = () => {
+let add = firstInput.value + secondInput.value
+thirdInput.value = add
+}
+
+const Sub = () => {
+   let sub = firstInput.value - secondInput.value
+   thirdInput.value = sub
+}
+
+const Mul = () => {
+   let mul = firstInput.value * secondInput.value
+   thirdInput.value = mul
+}
+
+const Div = () => {
+   let div = firstInput.value / secondInput.value
+   thirdInput.value = div
+}
+
+const Cler = () => {
+   firstInput.value = " "
+   secondInput.value = " "
+   thirdInput.value = " "
+}
+
+
+
+
